c387e13ecf5e27bc31cae5ea71b7d3ec8715a06f
DarylNyd <darylnyd@gmail.com>
2024-12-18 14:00:47 +0100

    feat(wip): add footer

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import style from './Main.module.scss';
 
+// Components
 import Home from '../Home/Home';
+import Footer from '../../components/partials/Footer/Footer';
 
-const Filter = () => {
+const Main = () => {
   return (
     <div className={style.wrapper}>
       <Home />
+      <Footer />
     </div>
   );
 };
 
-export default Filter;
+export default Main;
