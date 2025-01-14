ed09b4479593f133e093bc99981833497f954801
DarylNyd <darylnyd@gmail.com>
2025-01-14 22:23:00 +0100

    feat(wip): add pages

---
+++ src/pages/Track/Track.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from './Track.module.scss';
+
+// Components
+
+const Track = () => {
+  return (
+    <div className={style.container}>
+      <h1>Track</h1>
+    </div>
+  );
+};
+
+export default Track;
